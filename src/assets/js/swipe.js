73da913558f005bedac6f9b5796181523f1a0ceb
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2024-11-15 02:32:52 -0800

    Finished almost everythinng in the about page

--- src/assets/js/swipe.js
+++ src/assets/js/swipe.js
@@ -17,36 +17,38 @@
 
 "use strict";
 const d = document;
-d.addEventListener("DOMContentLoaded", function(event) {
-
-    if (d.querySelector('.headroom')) {
-        var headroom = new Headroom(document.querySelector("#navbar-main"), {
-            offset: 0,
-            tolerance: {
-                up: 0,
-                down: 0
-            },
-        });
-        headroom.init();
-    }
-
-    // Tooltips
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-
-    // Popovers
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-    })
-
-    var scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 500,
-        speedAsDuration: true
+d.addEventListener("DOMContentLoaded", function (event) {
+  if (d.querySelector(".headroom")) {
+    var headroom = new Headroom(document.querySelector("#navbar-main"), {
+      offset: 0,
+      tolerance: {
+        up: 0,
+        down: 0,
+      },
     });
+    headroom.init();
+  }
 
-    d.querySelector('.current-year').textContent = new Date().getFullYear();
+  // Tooltips
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
 
+  // Popovers
+  var popoverTriggerList = [].slice.call(
+    document.querySelectorAll('[data-toggle="popover"]')
+  );
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+  });
+
+  d.querySelector(".current-year").textContent = new Date().getFullYear();
 });
